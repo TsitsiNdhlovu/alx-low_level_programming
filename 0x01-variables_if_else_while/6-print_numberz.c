f4b591a0724806d2498b23f72589e9c34d51c06b
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-17 17:09:39 -0800

    Using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- print using putchar
+ * Return: Always 0 Success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
