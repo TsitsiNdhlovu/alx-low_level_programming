a8ceac96817e9621b3485e65740fdb080c683d0f
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-17 17:14:11 -0800

    Print in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabeth in reverse order
+ * Return: Always 0(Success)
+*/
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp <= 'a'; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
