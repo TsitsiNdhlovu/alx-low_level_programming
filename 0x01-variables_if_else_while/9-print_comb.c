17a39180949a4cb3f1b30c2162d21c3e8e4a62dc
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-17 17:27:26 -0800

    Spaced %

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all combination of two digits
+ * Return: Always 0(Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
