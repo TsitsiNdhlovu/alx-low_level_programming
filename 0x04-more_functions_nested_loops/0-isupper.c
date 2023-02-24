b86d79a033fffae9597a49a2641aa1c246ddca41
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-24 06:16:01 -0800

    Upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper- print uppercase character
+ * @n: number to be checked
+ * Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
