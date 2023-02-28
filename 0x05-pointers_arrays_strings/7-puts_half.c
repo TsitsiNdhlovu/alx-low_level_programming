73eda74c09e25a9e43e9f92fa316f73fbca47787
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-27 16:47:49 -0800

    Another try

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,28 +6,21 @@
 */
 void puts_half(char *str)
 {
-	int len = _strlen(str);
-	
-	if (len % 2 == 0)
-		len = len / 2;
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	if (!(len % 2))
+		len /= 2;
 	else
 		len = (len + 1) / 2;
-	while (str[len] != '\0')
+
+	while (*(str + len) != '\0')
 	{
-		_putchar(str[len]);
+		_putchar(*(str + len));
 		len++;
 	}
 	_putchar('\n');
-}
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-int _strlen(char *s)
-{
-	int count, inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-		inc++;
-	return (inc);
 }
