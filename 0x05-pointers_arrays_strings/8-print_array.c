c66f63bc8de345d8f01e751deb1402862e6851fd
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-02-27 16:53:49 -0800

    Fixed whitespace

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,23 @@
-
 #include "main.h"
+#include <stdio.h>
+
 /**
-*print_array - print n element of array
-*@a: array
-*@n: number of elements
-*Return: nothing
-*/
+ * print_array - prints an aray a number of times.
+ * @a: an array.
+ * @n: the number of elements to print.
+ *
+ */
 void print_array(int *a, int n)
 {
-	int inc;
-	for (inc = 0; inc < n ; inc++)
+	int i;
+
+	for (i = 0 ; i < n ; i++)
 	{
-		if (inc != n - 1)
-			printf("%d, ", a[inc]);
-		else
-			printf("%d", a[inc]);
+		printf("%d", *(a + i));
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
