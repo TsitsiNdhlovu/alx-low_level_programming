49a94e10f26f6f777b3002f13459a578c3cbf1f2
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-03-30 16:11:41 -0700

    Removed whitespace trailing in line 21

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * @h: Pointer to the list_t list
  *
  * Return: number of elements in h
  */
@@ -18,4 +18,4 @@ size_t list_len(const list_t *h)
 		h = h->next;
 	}
 	return (n);
-} 
+}
